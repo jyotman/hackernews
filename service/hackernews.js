0c725b79b2223557728edd7b9cba65fe85599a7b
Jyotman Singh <jyotman94@gmail.com>
2018-07-12 23:01:09 +0800

    hackernews API

---
+++ service/hackernews.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const request = require('request');
+
+const BASE_API = 'https://hacker-news.firebaseio.com/v0';
+
+exports.getTopStoryIds = () => {
+    return fetch('topstories.json')
+}
+
+exports.getStoryById = (storyId) => {
+    return fetch(`item/${storyId}.json`);
+}
+
+function fetch(path) {
+    const URL = `${BASE_API}/${path}`;
+    console.log(URL);
+    return new Promise((resolve, reject) => {
+        request(URL, { json: true }, (err, resp, body) => {
+            if (err !== null)
+                reject(err);
+            else if (resp.statusCode >= 300)
+                reject(body);
+            else
+                resolve(body);
+        });
+    })
+}
